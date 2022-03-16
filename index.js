143510ec7bd2d93353e046e4944bbf0bbf219a3f
adotahirovich <80359702+adotahirovich@users.noreply.github.com>
2022-03-16 01:55:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+  res.send("Welcome");
 });
 
 app.listen(process.env.PORT || 5000);
